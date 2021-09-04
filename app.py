ebd3e1ae38f2c4e94b88e567460b93c84f662358
Joe <joebreilly@yahoo.ie>
2021-09-04 10:45:00 +0000

    Turn off debug mode in app.py

--- app.py
+++ app.py
@@ -365,4 +365,4 @@ def is_authenticated():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
